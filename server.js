5e74a90b7d60aeeb45c0bc57a1f22898ddc11286
btchae <bchae@nyu.edu>
2016-06-16 12:16:00 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+// LISTEN
+app.listen(port);
+console.log('=============================');
+console.log('Server running off PORT: ' + port);
+console.log('=============================');
